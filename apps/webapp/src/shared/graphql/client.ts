5fc88d56fc6746722f5c6837b4d9742837e84a8e
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-03 20:35:46 +0200

    feature(webapp): updates GraphqlClient to use graphqlEnpoint value depending on env

--- apps/webapp/src/shared/graphql/client.ts
+++ apps/webapp/src/shared/graphql/client.ts
@@ -1,3 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
-
-export const graphqlClient = new GraphQLClient('http://localhost:3333/graphql');
+console.log(process.env['NODE_ENV']);
+const graphqlEndpoint =
+  process.env['NODE_ENV'] === 'production'
+    ? 'https://us-central1-feather-22710.cloudfunctions.net/api/graphql'
+    : 'http://localhost:3333/graphql';
+export const graphqlClient = new GraphQLClient(graphqlEndpoint);
