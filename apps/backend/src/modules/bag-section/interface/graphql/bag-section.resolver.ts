86b5264f60ee27051ed82ce2b350c008cf54abeb
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-07 20:19:45 +0200

    refactor(bag-section): moves graphql interface to its own folder

--- apps/backend/src/modules/bag-section/interface/graphql/bag-section.resolver.ts
+++ apps/backend/src/modules/bag-section/interface/graphql/bag-section.resolver.ts
@@ -1,15 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import {
-  GetBagSectionQuery,
-  GetBagSectionQueryResult,
-} from '../core/application/queries/get-bag-section/get-bag-section.query';
-import { UpdateBagSectionInput } from '../../../../graphql-generated-types';
+import { UpdateBagSectionInput } from '../../../../../graphql-generated-types';
+import { GraphQLReturn } from '../../../../../graphql-return.type';
 import {
   UpdateBagSectionCommand,
   UpdateBagSectionCommandResult,
-} from '../core/application/commands/update-bag-section/update-bag-section.command';
-import { GraphQLReturn } from '../../../../graphql-return.type';
+} from '../../core/application/commands/update-bag-section/update-bag-section.command';
+import {
+  GetBagSectionQuery,
+  GetBagSectionQueryResult,
+} from '../../core/application/queries/get-bag-section/get-bag-section.query';
 
 @Resolver('BagSection')
 export class BagSectionResolver {
