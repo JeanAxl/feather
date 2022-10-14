f60e6446621a19238524bb7a7d4ef0d3875449ff
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-14 20:07:19 +0200

    chore(sqlite): adds basic configuration for sqlite

--- apps/backend/src/modules/app/app.module.ts
+++ apps/backend/src/modules/app/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { BagSectionModule } from '../bag-section/bag-section.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      dropSchema: true,
+      synchornize: true,
+      entities: [],
+      autoloadEntities: true,
+      keepConnectionAlive: false,
     }),
     BagSectionModule,
   ],
