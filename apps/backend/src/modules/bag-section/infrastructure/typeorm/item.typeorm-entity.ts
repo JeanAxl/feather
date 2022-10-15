00d4ec77f2af916f188769245ef0fd42db887e82
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-15 17:55:44 +0200

    feature(bag-section): fixes ItemTypeOrmEntity

--- apps/backend/src/modules/bag-section/infrastructure/typeorm/item.typeorm-entity.ts
+++ apps/backend/src/modules/bag-section/infrastructure/typeorm/item.typeorm-entity.ts
@@ -1,10 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class ItemTypeOrmEntity {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
-  
+  id: string;
+
   @Column()
   name: string;
 
@@ -15,5 +15,5 @@ export class ItemTypeOrmEntity {
   quantity: number;
 
   @Column()
-  weight: number
+  weight: number;
 }
