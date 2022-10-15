434028d5dc03a3df1c97615200d4365bfc0301b6
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-15 17:54:31 +0200

    chore(testing): adds utils and module setup

--- apps/backend/src/modules/bag-section/bag-section.module.ts
+++ apps/backend/src/modules/bag-section/bag-section.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import {
   commandHandlers,
   queryHandlers,
 } from './core/application/bag-section.application';
 import { bagSectionInfrastructure } from './infrastructure/bag-section.infrastructure';
+import { ItemTypeOrmEntity } from './infrastructure/typeorm/item.typeorm-entity';
 import { BagSectionResolver } from './interface/bag-section.resolver';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, TypeOrmModule.forFeature([ItemTypeOrmEntity])],
   providers: [
     BagSectionResolver,
     ...queryHandlers,
--- apps/backend/src/utils/test/test.module.ts
+++ apps/backend/src/utils/test/test.module.ts
@@ -1,6 +1,7 @@
 import { Module, ModuleMetadata } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { QueryRunner } from 'typeorm';
 import { ItemTypeOrmEntity } from '../../modules/bag-section/infrastructure/typeorm/item.typeorm-entity';
 
 @Module({
@@ -10,13 +11,48 @@ import { ItemTypeOrmEntity } from '../../modules/bag-section/infrastructure/type
       database: ':memory:',
       dropSchema: true,
       entities: [ItemTypeOrmEntity],
+      synchronize: true,
       keepConnectionAlive: false,
     }),
   ],
 })
 class TestingModule {}
 
-const testingModuleFactory = async (
+export type Fixture = {
+  tableName: string;
+  items: any[];
+};
+
+export const loadFixtures = async (
+  queryRunner: QueryRunner,
+  fixtures: Fixture[]
+): Promise<void> => {
+  try {
+    await queryRunner.startTransaction();
+    for (const fixture of fixtures) {
+      try {
+        const repository = queryRunner.manager.getRepository(fixture.tableName);
+        const entitiesToSave = repository.create(fixture.items);
+        await queryRunner.manager.save(entitiesToSave);
+      } catch (error) {
+        console.log(error);
+        throw new Error(
+          `Error on loading fixture > Failed to load fixtures for entity "${
+            fixture.tableName
+          }" , "${JSON.stringify(fixture.items)}`
+        );
+      }
+    }
+    await queryRunner.commitTransaction();
+  } catch (error) {
+    await queryRunner.rollbackTransaction();
+    throw new Error(`Failed to setup fixtures ! Got error: ${error}`);
+  } finally {
+    await queryRunner.release();
+  }
+};
+
+export const testingModuleFactory = async (
   providers: ModuleMetadata['providers'],
   imports: ModuleMetadata['imports']
 ) => {
