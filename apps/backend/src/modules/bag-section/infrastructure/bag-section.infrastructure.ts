98b83f66e4821cc421339a6c889da6b387f89cce
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-15 17:55:16 +0200

    feature(bag-section): remove BagSectionRepository

--- apps/backend/src/modules/bag-section/infrastructure/bag-section.infrastructure.ts
+++ apps/backend/src/modules/bag-section/infrastructure/bag-section.infrastructure.ts
@@ -2,11 +2,9 @@ import { BagSectionRepositoryPort } from '../core/domain/ports/bag-section-repos
 import { ItemRepositoryPort } from '../core/domain/ports/item-repository.port';
 import { BagSectionInMemoryRepository } from './in-memory/bag-section.in-memory-repository';
 import { ItemRepositoryAdapter } from './typeorm/item.repository-adapter';
-import { ItemRepository } from './typeorm/item.typeorm-repository';
 
 export const bagSectionInfrastructure = {
   providers: [
-    ItemRepository,
     {
       provide: BagSectionRepositoryPort,
       useClass: BagSectionInMemoryRepository,
--- apps/backend/src/modules/bag-section/infrastructure/typeorm/item.typeorm-repository.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from 'typeorm';
-import { ItemTypeOrmEntity } from './item.typeorm-entity';
-
-export class ItemRepository extends Repository<ItemTypeOrmEntity> {}
