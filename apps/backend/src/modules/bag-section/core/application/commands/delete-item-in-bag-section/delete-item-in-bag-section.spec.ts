84c39bcbb046bbd49425ededb9ff718686fbb6b9
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-15 17:57:46 +0200

    test(bag-section): integrates test utils in DeleteItemInBagSection test

--- apps/backend/src/modules/bag-section/core/application/commands/delete-item-in-bag-section/delete-item-in-bag-section.spec.ts
+++ apps/backend/src/modules/bag-section/core/application/commands/delete-item-in-bag-section/delete-item-in-bag-section.spec.ts
@@ -1,29 +1,53 @@
-import { ItemInMemoryRepository } from '../../../../infrastructure/in-memory/item.in-memory-repository';
+import { DataSource, Repository } from 'typeorm';
+import {
+  loadFixtures,
+  testingModuleFactory,
+} from '../../../../../../utils/test/test.module';
+import { BagSectionModule } from '../../../../bag-section.module';
+import { ItemTypeOrmEntity } from '../../../../infrastructure/typeorm/item.typeorm-entity';
 import { DeleteItemInBagSectionCommand } from './delete-item-in-bag-section.command';
 import { DeleteItemInBagSectionCommandHandler } from './delete-item-in-bag-section.command-handler';
 
 describe('DeleteItemInBagSectionCommandHandler', () => {
+  let commandHandler: DeleteItemInBagSectionCommandHandler;
+  let itemRepository: Repository<ItemTypeOrmEntity>;
+  beforeEach(async () => {
+    const module = await testingModuleFactory([], [BagSectionModule]);
+    const dataSource = module.get(DataSource);
+    const queryRunner = dataSource.createQueryRunner();
+    await loadFixtures(queryRunner, [
+      {
+        tableName: 'ItemTypeOrmEntity',
+        items: [
+          {
+            id: 1,
+            name: 'someName',
+            description: 'someDescription',
+            weight: 1,
+            quantity: 1,
+          },
+          {
+            id: '2',
+            name: 'someName',
+            description: 'someDescription',
+            weight: 1,
+            quantity: 1,
+          },
+        ],
+      },
+    ]);
+    commandHandler = module.get(DeleteItemInBagSectionCommandHandler);
+    itemRepository = dataSource.getRepository(ItemTypeOrmEntity);
+  });
+
   it('should remove an item by id', async () => {
-    const itemId = 'itemId';
+    const itemId = '2';
     const command = new DeleteItemInBagSectionCommand({
       itemId,
     });
-    const itemRepository = new ItemInMemoryRepository();
-    itemRepository.feedWith([
-      {
-        id: 'itemId',
-        name: 'someName',
-        description: 'someDescription',
-        weight: 1,
-        quantity: 1,
-      },
-    ]);
-    const commandHandler = new DeleteItemInBagSectionCommandHandler(
-      itemRepository
-    );
     await commandHandler.execute(command);
 
-    const items = itemRepository.items;
+    const items = await itemRepository.find();
     expect(items.find((item) => item.id === itemId)).toBeUndefined();
   });
 });
