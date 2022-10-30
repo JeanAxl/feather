406e20d43ea4ec7274d663ed9abe065cb74325ac
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-10-30 10:14:13 +0100

    feature(backend): adds basic uuid validation in command

--- apps/backend/src/modules/bag-section/core/application/commands/add-item-in-bag-section/add-item-in-bag-section.command.ts
+++ apps/backend/src/modules/bag-section/core/application/commands/add-item-in-bag-section/add-item-in-bag-section.command.ts
@@ -1,10 +1,15 @@
 import { ICommand } from '@nestjs/cqrs';
+import * as uuid from 'uuid';
 import { Item } from '../../../domain/item.entity';
 
 type AddItemInBagSectionCommandPayload = NonNullable<Partial<Item>>;
 
 export class AddItemInBagSectionCommand implements ICommand {
-  constructor(public readonly payload: AddItemInBagSectionCommandPayload) {}
+  constructor(public readonly payload: AddItemInBagSectionCommandPayload) {
+    if (!uuid.validate(payload.id)) {
+      throw new Error('api.bag-section.invalida_uuid');
+    }
+  }
 }
 
 export type AddItemInBagSectionCommandResult = void;
