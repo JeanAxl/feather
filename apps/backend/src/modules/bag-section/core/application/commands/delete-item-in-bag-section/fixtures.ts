c6128202524c077530a239b5d296dc19d103b208
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-11-03 17:12:30 +0100

    chore(): fixes fixtures

--- apps/backend/src/modules/bag-section/core/application/commands/delete-item-in-bag-section/fixtures.ts
+++ apps/backend/src/modules/bag-section/core/application/commands/delete-item-in-bag-section/fixtures.ts
@@ -1,26 +1,10 @@
+import { v4 as uuid } from 'uuid';
 import { itemFixtureFactory } from '../../../../infrastructure/typeorm/item/item.fixture-factory';
 import { ItemTypeOrmEntity } from '../../../../infrastructure/typeorm/item/item.typeorm-entity';
-
+export const itemId = uuid();
 export const fixtures = [
   {
     tableName: ItemTypeOrmEntity.name,
-    items: [
-      itemFixtureFactory({}),
-      itemFixtureFactory({}),
-      {
-        id: 1,
-        name: 'someName',
-        description: 'someDescription',
-        weight: 1,
-        quantity: 1,
-      },
-      {
-        id: '2',
-        name: 'someName',
-        description: 'someDescription',
-        weight: 1,
-        quantity: 1,
-      },
-    ],
+    items: [itemFixtureFactory({ id: itemId }), itemFixtureFactory({})],
   },
 ];
