83261f3925fad4c87c990de1fb4b3548e6d0befc
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-11-09 19:37:13 +0100

    test(bag-section): adds test for DeleteBagSectionCommandHandler

--- apps/backend/src/modules/bag-section/core/application/commands/delete-bag-section/delete-bag-section.spec.ts
+++ apps/backend/src/modules/bag-section/core/application/commands/delete-bag-section/delete-bag-section.spec.ts
@@ -1,19 +1,37 @@
 import { TestingModule } from '@nestjs/testing';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { testingModuleFactory } from '../../../../../../shared/test/test.module';
 import { BagSectionModule } from '../../../../bag-section.module';
+import { BagSectionTypeOrmEntity } from '../../../../infrastructure/typeorm/bag-section/bag-section.typeorm-entity';
+import { DeleteBagSectionCommand } from './delete-bag-section.command';
 import { DeleteBagSectionCommandHandler } from './delete-bag-section.command-handler';
+import { bagSectionId, fixtures } from './fixtures';
 describe('DeleteBagSectionCommandHandler', () => {
   let handler: DeleteBagSectionCommandHandler;
+  let bagSectionRepository: Repository<BagSectionTypeOrmEntity>;
   let dataSource: DataSource;
   let module: TestingModule;
 
   beforeAll(async () => {
-    [module, dataSource] = await testingModuleFactory([BagSectionModule], []);
+    [module, dataSource] = await testingModuleFactory(
+      [BagSectionModule],
+      fixtures
+    );
     handler = module.get(DeleteBagSectionCommandHandler);
+    bagSectionRepository = dataSource.getRepository(BagSectionTypeOrmEntity);
   });
 
   afterAll(async () => {
     await dataSource.destroy();
+  });
+
+  it('should delete a BagSection', async () => {
+    const before = await bagSectionRepository.findOneBy({ id: bagSectionId });
+    expect(before).not.toBeNull();
+    const command = new DeleteBagSectionCommand({ id: bagSectionId });
+    console.log(handler);
+    await handler.execute(command);
+    const result = await bagSectionRepository.findOneBy({ id: bagSectionId });
+    expect(result).toBeNull();
   });
 });
---
+++ apps/backend/src/modules/bag-section/core/application/commands/delete-bag-section/fixtures.ts
@@ -0,0 +1,10 @@
+import { v4 as uuid } from 'uuid';
+import { bagSectionFixtureFactory } from '../../../../infrastructure/typeorm/bag-section/bag-section.fixture-factory';
+import { BagSectionTypeOrmEntity } from '../../../../infrastructure/typeorm/bag-section/bag-section.typeorm-entity';
+export const bagSectionId = uuid();
+export const fixtures = [
+  {
+    tableName: BagSectionTypeOrmEntity.name,
+    items: [bagSectionFixtureFactory({ id: bagSectionId })],
+  },
+];
