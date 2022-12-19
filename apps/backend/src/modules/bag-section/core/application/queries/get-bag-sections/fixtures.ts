6f6b484d92882856d7cf50a7796b3c2103c31896
Axel Bie <50874689+JeanAxl@users.noreply.github.com>
2022-12-19 14:53:55 +0700

    wip

--- apps/backend/src/modules/bag-section/core/application/queries/get-bag-sections/fixtures.ts
+++ apps/backend/src/modules/bag-section/core/application/queries/get-bag-sections/fixtures.ts
@@ -3,18 +3,28 @@ import { bagSectionFixtureFactory } from '../../../../infrastructure/typeorm/bag
 import { BagSectionTypeOrmEntity } from '../../../../infrastructure/typeorm/bag-section/bag-section.typeorm-entity';
 import { itemFixtureFactory } from '../../../../infrastructure/typeorm/item/item.fixture-factory';
 import { ItemTypeOrmEntity } from '../../../../infrastructure/typeorm/item/item.typeorm-entity';
+import { unitFixtureFactory } from '../../../../infrastructure/typeorm/unit/unit.fixture-factory';
+import { UnitTypeOrmEntity } from '../../../../infrastructure/typeorm/unit/unit.typeorm-entity';
 const bagSectionId = uuid();
+const unitId = uuid();
+export const unit = unitFixtureFactory({
+  id: unitId,
+  label: 'kg',
+  name: 'kilogramme',
+});
+export const bagSection = bagSectionFixtureFactory({ id: bagSectionId });
+export const item = itemFixtureFactory({ bagSectionId, unitId });
 export const fixtures = [
   {
     tableName: BagSectionTypeOrmEntity.name,
-    items: [bagSectionFixtureFactory({ id: bagSectionId })],
+    items: [bagSection],
+  },
+  {
+    tableName: UnitTypeOrmEntity.name,
+    items: [unit],
   },
   {
     tableName: ItemTypeOrmEntity.name,
-    items: [
-      itemFixtureFactory({ bagSectionId }),
-      itemFixtureFactory({ bagSectionId }),
-      itemFixtureFactory({ bagSectionId }),
-    ],
+    items: [item],
   },
 ];
